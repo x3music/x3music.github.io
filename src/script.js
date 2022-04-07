e7326710bb1d3b393438fd83d45a3bd4e65d648e
x3music <103132953+x3music@users.noreply.github.com>
2022-04-07 15:11:56 -0500

    added artists array, cut to only songindex value

--- src/script.js
+++ src/script.js
@@ -10,25 +10,28 @@ const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 
 // song titles and file names
-const songs = ['kiss me more - doja cat ft sza', "the perfect girl - &#10;mareux", 'smokin out the window - silk sonic', 'boyfriend - dove cameron', 'middle of the night - elley duhé', 'leave the door open - silk sonic', 'heat waves - glass animals'];
+const songs = ['kiss me more', "the perfect girl", 'smokin out the window', 'boyfriend', 'middle of the night', 'leave the door open', 'heat waves'];
+const artists = ['doja cat ft sza', 'mareux', 'silk sonic', 'dove cameron', 'elley duhé', 'silk sonic', 'glass animals'];
 const songFiles = ['kiss-me-more_doja-cat_sza', 'the-perfect-girl_mareux', 'smokin-out-the-window_silk-sonic', 'boyfriend_dove-cameron', 'middle-of-the-night_elley-duhe', 'leave-the-door-open_silk-sonic', 'heat-waves_glass-animals'];
 
 // increments song
 let songIndex = 1;
-let songFileIndex = 1;
 
 // loads stuff
-loadSong(songs[songIndex], songFiles[songFileIndex]);
+loadSong(songFiles[songIndex]);
 
 // updates song info
-function loadSong(song, songFile) {
-  title.innerText = song;
+function loadSong(songFile) 
+{
+  title.innerText = songs[songIndex];
+  artist.innerText = artists[songIndex];
   audio.src = `../songs/pop-songs/${songFile}.mp3`;
   cover.src = `../album-covers/pop-album-covers/${songFile}.jpg`;
 }
 
 // func: play song
-function playSong() {
+function playSong() 
+{
   musicContainer.classList.add('play');
   playBtn.querySelector('i.fas').classList.remove('fa-play');
   playBtn.querySelector('i.fas').classList.add('fa-pause');
@@ -37,7 +40,8 @@ function playSong() {
 }
 
 // func: pause song
-function pauseSong() {
+function pauseSong() 
+{
   musicContainer.classList.remove('play');
   playBtn.querySelector('i.fas').classList.add('fa-play');
   playBtn.querySelector('i.fas').classList.remove('fa-pause');
@@ -46,43 +50,35 @@ function pauseSong() {
 }
 
 // func: prev song
-function prevSong() {
+function prevSong() 
+{
   songIndex--;
-  songFileIndex--;
 
   if(songIndex < 0) 
   {
     songIndex = songs.length - 1;
   }
-  if(songFileIndex < 0)
-  {
-    songFileIndex = songFiles.length - 1;
-  }
-
-  loadSong(songs[songIndex], songFiles[songFileIndex]);
+  
+  loadSong(songFiles[songIndex]);
   playSong();
 }
 
 // func: next song
 function nextSong() {
   songIndex++;
-  songFileIndex++;
 
   if(songIndex > songs.length - 1)
   {
     songIndex = 0;
   }
-  if(songFileIndex > songFiles.length - 1)
-  {
-    songFileIndex = 0;
-  }
   
-  loadSong(songs[songIndex], songFiles[songFileIndex]);
+  loadSong(songs[songIndex]);
   playSong();
 }
 
 // updates progress bar
-function updateProgress(e) {
+function updateProgress(e) 
+{
   const { duration, currentTime } = e.srcElement;
   const progressPercent = (currentTime / duration) * 100;
   progress.style.width = `${progressPercent}%`;
